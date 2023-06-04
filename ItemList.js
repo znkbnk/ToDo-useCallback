d29d84d2dd96a0aca3b19041248a99d89658ecdc
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-04 19:39:17 +0100

    Create ItemList.js

---
+++ ItemList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ItemList({ items }) {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default ItemList;
